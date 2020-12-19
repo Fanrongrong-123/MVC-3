2cdf7a60c899597c1b2c295a2b78fc2c35f20fa2
Fanrr <2514509399@qq.com>
2020-12-20 00:11:39 +0800

    MVC

--- src/app1.js
+++ src/app1.js
@@ -1,38 +1,81 @@
-import $ from 'jquery' //引入jquery
+import $, { contains } from 'jquery' //引入jquery
+
+
 console.log($)
 
-const $button1 = $('#add1')
-const $button2 = $('#minus1')
-const $button3 = $('#mul2')
-const $button4 = $('#divide2')
-const $number = $('#number')
-const n = localStorage.getItem("n")
-$number.text(n || 100)
+const m = {
+    data: {
+        n: localStorage.getItem("n")
+    }
+}
 
-$button1.on('click', () => {
-    let n = parseInt($number.text()) //parseInt(string, radix)解析一个字符串并返回指定基数的十进制整数
-    n += 1
-    localStorage.setItem("n", n)
-    $number.text(n)
-})
+const v = {
+    html: `
+    <section class="app1">
+                <div id="number">
+                    <span>{{n}}</span>
+                </div>
+                <div class="actions">
+                    <button id="add1">+1</button>
+                    <button id="minus1">-1</button>
+                    <button id="mul2">*2</button>
+                    <button id="divide2">÷2</button>
+                </div>
+            </section>
+    `,
+    update() {
+        c.ui.number.text(m.data.n || 100)
+    },
+    render() {//函数
+        const $element = $(v.html.replace('{{n}}', m.data.n))
+            .appendTo('body>.page')
+    }
 
-$button2.on('click', () => {
-    let n = parseInt($number.text())
-    n -= 1
-    localStorage.setItem("n", n)
-    $number.text(n)
-})
+}
+//第一次渲染HTML
+v.render()
 
-$button3.on('click', () => {
-    let n = parseInt($number.text())
-    n *= 2
-    localStorage.setItem("n", n)
-    $number.text(n)
-})
+const c = {
+    ui: {
+        button1: $('#add1'),
+        button2: $('#minus1'),
+        button3: $('#mul2'),
+        button4: $('#divide2'),
+        number: $('#number'),
+    },
 
-$button4.on('click', () => {
-    let n = parseInt($number.text())
-    n = n / 2
-    localStorage.setItem("n", n)
-    $number.text(n)
-})
+
+    bindEvents() {
+        c.ui.button1.on('click', () => {
+            let n = parseFloat(c.ui.number.text()) //函数解析一个参数（必要时先转换为字符串）并返回一个浮点数
+            n += 1
+            localStorage.setItem("n", n)
+            c.ui.number.text(n)
+        })
+
+        c.ui.button2.on('click', () => {
+            let n = parseFloat(c.ui.number.text())
+            n -= 1
+            localStorage.setItem("n", n)
+            c.ui.number.text(n)
+        })
+
+        c.ui.button3.on('click', () => {
+            let n = parseFloat(c.ui.number.text())
+            n *= 2
+            localStorage.setItem("n", n)
+            c.ui.number.text(n)
+        })
+
+        c.ui.button4.on('click', () => {
+            let n = parseFloat(c.ui.number.text())
+            n = n / 2
+            localStorage.setItem("n", n)
+            c.ui.number.text(n)
+        })
+    }
+}
+c.bindEvents()
+
+
+
--- src/app2.js
+++ src/app2.js
@@ -1,12 +1,31 @@
 import $ from "jquery"
 import './app2.css'
 
+const html = `
+<section id="app2">
+            <ol class="tab-bar">
+                <li><span>111</span></li>
+                <li><span>2222</span></li>
+            </ol>
+            <ol class="tab-content">
+                <li>内容1</li>
+                <li>内容2</li>
+            </ol>
+        </section>
+`
+const $html = $(html).appendTo('body>.page')
+
 const $tabBar = $("#app2 .tab-bar")
 const $tabContent = $("#app2 .tab-content")
+
+const localKey = 'app2.index'
+const index = localStorage.getItem(localKey) || 0
 
 $tabBar.on('click', 'li', (e) => {
     const $li = $(e.currentTarget)
     const index = $li.index()
+
+    localStorage.setItem(localKey, index)
 
     $li.addClass('selected')
         .siblings()
@@ -17,4 +36,4 @@ $tabBar.on('click', 'li', (e) => {
         .siblings().removeClass('active')
 })
 
-$tabBar.children().eq(0).trigger('click') //触发自动触发click事件
+$tabBar.children().eq(index).trigger('click') //触发自动触发click事件
